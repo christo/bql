07c4a1316ff7beb17769f0fdfb51c615b3883c39
cmountford <cmountford@atlassian.com>
2011-06-16 17:51:36 +1000

    Edit from the UI

--- js/bql/main.js
+++ js/bql/main.js
@@ -7,7 +7,6 @@ var $ = require('speakeasy/jquery').jQuery;
 var img = require('speakeasy/resources').getImageUrl(module, 'projectavatar.png');
 
 $(document).ready(function() {
-    alert("removing jqlform");
     $('#jqlform').css("display", "none");
     var leftHandColumn = $("#iss-wrap");
     if (leftHandColumn.hasClass("expanded")){
