b1dec98abcb2a4a853c4394da84664215a8f2e3a
Chris Mountford <chris@think.net.au>
2011-06-16 16:44:40 +1000

    removing jql text area

--- js/bql/main.js
+++ js/bql/main.js
@@ -7,5 +7,5 @@ var $ = require('speakeasy/jquery').jQuery;
 var img = require('speakeasy/resources').getImageUrl(module, 'projectavatar.png');
 
 $(document).ready(function() {
-    $('body').prepend("<h1 id='foo'><img src='" + img + "'>Hi</h1><h1 id='bar'>Bye</h1>");
+    $('jqlform').css("display", "none");
 });
